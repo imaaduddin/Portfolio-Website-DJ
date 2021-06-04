edf31032da477e5d936e7850d94c1e7ec3e3345e
Imaad Uddin <imaaduddin@icloud.com>
2021-06-03 19:50:36 -0500

    figuring flask out lol

--- server.py
+++ server.py
@@ -4,14 +4,10 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 print(__name__)
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-  return render_template("index.htm", name=username, post_id=post_id)
+@app.route("/")
+def my_home():
+  return render_template("index.htm")
 
-@app.route("/about.htm")
-def about():
-  return render_template("about.htm")
-
-@app.route("/blog")
-def blog():
-  return "This is a nice blog!"
+@app.route("/<string:page_name>")
+def html_page(page_name):
+  return render_template(page_name)
