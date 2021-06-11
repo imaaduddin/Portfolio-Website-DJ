79beaac505a98bca252fae0b6906b6077dc59636
Imaad Uddin <imaaduddin@icloud.com>
2021-06-10 20:56:10 -0500

    forms

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from os import name
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import csv
 
 app = Flask(__name__)
 print(__name__)
@@ -12,6 +13,26 @@ def my_home():
 def html_page(page_name):
   return render_template(page_name)
 
+def write_to_file(data):
+  with open("database.txt", mode="a") as database:
+    email = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    file = database.write(f"\n{email},{subject},{message}")
+
+def write_to_csv(data):
+  with open("database.csv", mode="a") as database2:
+    email = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    csv_writer = csv.writer(database2, delimiter=",", quotechar="", quoting = csv.QUOTE_MINIMAL)
+
 @app.route("/submit_form", methods=["POST", "GET"])
 def submit_form():
-  return "form submitted yeahhh!!"
+  if request.method == "POST":
+    data = request.form.to_dict()
+    write_to_file(data)
+    print(data)
+    return redirect("/thankyou.htm")
+  else:
+    return "something went wrong. try again!"
